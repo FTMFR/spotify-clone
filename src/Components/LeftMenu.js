7ae54020975e7330786b59ac8dcb903f114ef0bc
FTMFR <fatemefarajzade018@gmail.com>
2023-11-20 21:48:26 +0330

    add active classlist and css to it

---
+++ src/Components/LeftMenu.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../Styles/LeftMenu.css';
+import { FaSpotify, FaEllipsis } from "react-icons/fa6";
+import { BiSearchAlt } from "react-icons/bi";
+import { Menu } from './Menu';
+import { MenuList } from './MenuList';
+
+
+const LeftMenu = () => {
+  return (
+    <div className='leftMenu'>
+      <div className='logoContainer'>
+        <i>
+          <FaSpotify />
+        </i>
+        <h2>Spotify</h2>
+        <i>
+          <FaEllipsis />
+        </i>
+      </div>
+      <div className='searchBox'>
+        <input type='text' placeholder='Search...' />
+        <i className='searchIcon'>
+          <BiSearchAlt />
+        </i>
+      </div>
+      <Menu title={'Menu'} menuObject={MenuList} />
+    </div>
+  );
+};
+
+export { LeftMenu };
