46f56fee6d39c176d3a29d4f40dd7c18be818ddb
FTMFR <fatemefarajzade018@gmail.com>
2023-11-26 21:52:53 +0330

    right menu done

--- src/Components/RightMenu.js
+++ src/Components/RightMenu.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import '../Styles/RightMenu.css';
+import { FaBell, FaCrown, FaRegHeart, FaSun,  } from "react-icons/fa6";
+import { FaCog } from 'react-icons/fa';
+import Profile from '../img/profile.jpg';
 
 
 const RightMenu = () => {
   return (
     <div className='rightMenu'>
-      
+      <div className='goPro'>
+        <i>
+          <FaCrown />
+          <p>
+            Go <span>Pro</span>
+          </p>
+        </i>
+        <i>
+          <FaBell />
+        </i>
+        <i>
+          <FaRegHeart />
+        </i>
+      </div>
+      <div className='profile'>
+        <i>
+          <FaSun />
+        </i>
+        <i>
+          <FaCog />
+        </i>
+        <div className='profileImage'>
+          <img src={Profile} alt=''  />
+        </div>
+      </div>
     </div>
   );
 };
 
-export {RightMenu};
+export { RightMenu };
